25caf5524eb4eb63f2c4315229abe87ccdf5d1e4
rilonmaharjan <rilon.maharjan@gmail.com>
2024-12-04 10:48:24 +0545

    [added status bar color] [manged device orientation]

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:cuplex/views/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.black,
+    statusBarColor: Colors.black,
+  ));
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      
       designSize: const Size(375, 813),
       minTextAdapt: true,
       splitScreenMode: true,
