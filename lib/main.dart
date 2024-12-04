fec92a588529174e54934007742cbcfd578fff3e
rilonmaharjan <rilon.maharjan@gmail.com>
2024-12-04 14:29:30 +0545

    [added :: transition right to left]

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class MyApp extends StatelessWidget {
         return GetMaterialApp(
           title: 'Cuplex',
           debugShowCheckedModeBanner: false,
+          defaultTransition: Transition.rightToLeft,
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
